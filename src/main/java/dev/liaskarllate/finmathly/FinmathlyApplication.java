b4a87516722b65d03e8b3a08af8076734a54ec82
LiaSkarllate <liaskarllate@protonmail.com>
2024-08-22 17:42:46 -0300

    Initial commit

---
+++ src/main/java/dev/liaskarllate/finmathly/FinmathlyApplication.java
@@ -0,0 +1,13 @@
+package dev.liaskarllate.finmathly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinmathlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinmathlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/liaskarllate/finmathly/FinmathlyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.liaskarllate.finmathly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinmathlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
