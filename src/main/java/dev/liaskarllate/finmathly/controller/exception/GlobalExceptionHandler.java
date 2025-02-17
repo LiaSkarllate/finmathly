631d0b08b59d6d37e9e7557a2690a19a38b7fc38
LiaSkarllate <liaskarllate@protonmail.com>
2025-02-16 21:15:05 -0300

    chore: update error message

--- src/main/java/dev/liaskarllate/finmathly/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/liaskarllate/finmathly/controller/exception/GlobalExceptionHandler.java
@@ -15,7 +15,7 @@ public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        StringBuilder errorsMessage = new StringBuilder("We noticed some issues with what you submitted. Please review the errors below.");
+        StringBuilder errorsMessage = new StringBuilder("We noticed some issues with what you submitted. Please review the following errors: ");
         
         for (FieldError error : ex.getBindingResult().getFieldErrors()) {
             errorsMessage.append(String.format("[%s: %s] ", error.getField(), error.getDefaultMessage()));
