e01630df29454342ef6b2fac778cf3187bc3a5ef
LiaSkarllate <liaskarllate@protonmail.com>
2025-09-24 01:29:04 -0300

    fix(backend): remove unnecessary @Component annotation from sorting field enums

--- backend/src/main/java/dev/liaskarllate/finmathly/asset/query/AssetSearchSortingField.java
+++ backend/src/main/java/dev/liaskarllate/finmathly/asset/query/AssetSearchSortingField.java
@@ -1,8 +1,5 @@
 package dev.liaskarllate.finmathly.asset.query;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public enum AssetSearchSortingField {
     name,
     modalityName,
--- backend/src/main/java/dev/liaskarllate/finmathly/modality/query/ModalitySearchSortingField.java
+++ backend/src/main/java/dev/liaskarllate/finmathly/modality/query/ModalitySearchSortingField.java
@@ -1,8 +1,5 @@
 package dev.liaskarllate.finmathly.modality.query;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public enum ModalitySearchSortingField {
     name;
 }
